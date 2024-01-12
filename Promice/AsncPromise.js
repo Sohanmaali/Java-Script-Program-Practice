c6839fe207403ba432aa4d59b9ef177eb42d0fcb
Sohan maali <sohanmaali4@gmail.com>
2024-01-12 22:46:07 +0530

    file add

---
+++ Promice/AsncPromise.js
@@ -0,0 +1,37 @@
+// const f1 = ()=>{
+//      console.log("Inside f1");
+//      setTimeout(() => {
+//           console.log("Inside set timeOut");
+//      }, 3000);
+// }
+
+// const f2 = ()=>{
+//      console.log("I am f2");
+// }
+
+// f1();
+// f2();
+
+
+
+
+const f1 = () => {
+     return new Promise((resolve, reject) => {
+          console.log("Inside f1");
+          setTimeout(() => {
+               console.log("Inside set timeOut");
+               resolve();
+          }, 3000);
+     })
+}
+
+const f2 = () => {
+     console.log("I am f2");
+}
+
+f1().then(() => {
+     console.log("I am then block");
+}).catch(() => {
+     console.log("I am catch block");
+})
+f2();
---
+++ Promice/First.js
@@ -0,0 +1,15 @@
+let first = new Promise((resolve, reject)=>{
+     // console.log("I am promise");
+     if(10%2 != 0){
+          resolve("This is even")
+     }else{
+          reject("This is odd")
+     }
+});
+
+
+first.then((result)=>{
+     console.log(result);
+}).catch((result)=>{
+     console.log(result);
+})
